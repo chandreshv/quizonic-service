18cf40c5ce8a5d1853aadafebe0af60784b59525
Chandresh Vashistha <mail2chandreshv@gmail.com>
2025-02-26 22:24:21 +0530

    Fixed tests by moving from SpringBootTest to WebMvcTest for unit testing

--- src/main/java/com/chandresh/quizonic/home/controller/HomeController.java
+++ src/main/java/com/chandresh/quizonic/home/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.chandresh.quizonic.home.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HomeController {
 
-  @RequestMapping("/")
+  @GetMapping("/")
   public ResponseEntity<String> home() {
     return ResponseEntity.ok("Welcome to Quizonic!");
   }
