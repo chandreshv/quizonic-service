1035f83023aee69dd892483dfcccb4d703370a26
Chandresh Vashistha <mail2chandreshv@gmail.com>
2025-02-26 22:28:40 +0530

    Maven spotless fix

--- src/test/java/com/chandresh/quizonic/config/SecurityConfigTest.java
+++ src/test/java/com/chandresh/quizonic/config/SecurityConfigTest.java
@@ -3,13 +3,11 @@ package com.chandresh.quizonic.config;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.chandresh.quizonic.home.controller.HomeController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.web.servlet.MockMvc;
