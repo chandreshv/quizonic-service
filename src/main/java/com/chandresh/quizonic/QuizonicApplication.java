b0274d862245b748649a88302f8ca965335e1893
Chandresh Vashistha <mail2chandreshv@gmail.com>
2025-03-09 06:30:00 +0530

    Removed datasource configuration as we are not using atm

--- src/main/java/com/chandresh/quizonic/QuizonicApplication.java
+++ src/main/java/com/chandresh/quizonic/QuizonicApplication.java
@@ -2,8 +2,9 @@ package com.chandresh.quizonic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class QuizonicApplication {
 
   public static void main(String[] args) {
